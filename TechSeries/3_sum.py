fd46748553a91b520ced74e4289f454a39272bec
spark <ses8621@gmail.com>
2020-01-24 16:27:26 -0800

    add 3_sum.py

---
+++ TechSeries/3_sum.py
@@ -0,0 +1,29 @@
+
+# LeetCode 15
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            # start two sum
+            j = i + 1
+            k = len(nums)-1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] == 0:
+                    res.append([nums[i],nums[j],nums[k]])
+                    while j < k and nums[j] == nums[j+1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k-1]:
+                        k -= 1
+                    j += 1
+                    k -= 1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                else:
+                    j += 1
+            # end two sum
+        return res
